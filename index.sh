2e38cb6abf33ab38a917f7d9c4df8b4b0b617b0d
JuliusOk <juliuzkurut@gmail.com>
2019-07-08 10:30:34 +0300

    added sorting non static data

--- index.sh
+++ index.sh
@@ -47,33 +47,65 @@
 # Sorting the array in Bash 
 # using Bubble sort 
 
-# Static input of Array
-echo "enter the number of elements to be in the array" 
+# # Static input of Array
+# echo "enter the number of elements to be in the array" 
+# read n
+
+# arr=(10 8 20 100 12)
+
+# echo "Array in original order"
+# echo ${arr[*]} 
+
+# # Performing Bubble sort 
+# for ((i = 0; i<5; i++)) 
+# do
+	
+# 	for((j = i; j<5-i-1; j++)) 
+# 	do
+	
+# 		if ((${arr[j]} > ${arr[$((j+1))]})) 
+# 		then
+# 			# swap 
+# 			temp=${arr[$j]} 
+# 			arr[$j]=${arr[$((j+1))]} 
+# 			arr[$((j+1))]=$temp 
+# 		fi
+# 	done
+# done
+
+# echo "Array in sorted order :"
+# echo ${arr[*]} 
+
+
+echo "enter maximum number of elements in the array"
 read n
-
-arr=(10 8 20 100 12)
-
-echo "Array in original order"
-echo ${arr[*]} 
-
-# Performing Bubble sort 
-for ((i = 0; i<5; i++)) 
+# taking input from user
+echo "enter  Numbers in array:"
+for (( i = 0; i < $n; i++ ))
 do
-	
-	for((j = i; j<5-i-1; j++)) 
-	do
-	
-		if ((${arr[j]} > ${arr[$((j+1))]})) 
-		then
-			# swap 
-			temp=${arr[$j]} 
-			arr[$j]=${arr[$((j+1))]} 
-			arr[$((j+1))]=$temp 
-		fi
-	done
+read nos[$i]
 done
-
-echo "Array in sorted order :"
-echo ${arr[*]} 
-
-
+#printing the number before sorting
+echo "  Numbers in an array are:"
+for (( i = 0; i < $n; i++ ))
+do
+echo ${nos[$i]}
+done
+# Now do the Sorting of numbers
+for (( i = 0; i < $n ; i++ ))
+do
+for (( j = $i; j < $n; j++ ))
+do
+if [ ${nos[$i]} -lt ${nos[$j]}  ]; then
+t=${nos[$i]}
+nos[$i]=${nos[$j]}
+nos[$j]=$t
+fi
+done
+done
+# Printing the sorted number in descending order
+echo -e "\nSorted Numbers "
+for (( i=0; i < $n; i++ ))
+do
+echo ${nos[$i]}
+done
